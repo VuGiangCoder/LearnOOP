34c1023314d387fb7eac43424feaf759eff29d58
VuGiangCoder <giang2010gc@gmail.com>
2022-07-09 15:56:45 +0700

    Update done!!

--- src/Fibonancci.java
+++ src/Fibonancci.java
@@ -13,14 +13,15 @@ public class Fibonancci {
             return -1;
         } else if (n == 0 || n == 1) {
             return n;
-        } else if (n > 9223372036854775807L) {
-            return 9223372036854775807L;
         } else {
             for (int i = 2; i < n; i++) {
                 f0 = f1;
                 f1 = fn;
                 fn = f0 + f1;
             }
+        }
+        if (fn >= 9223372036854775807L) {
+            fn = Long.MAX_VALUE;
         }
         return fn;
     }
--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,140 @@
 public class Solution {
+    private int numerator;
+    private int denominator;
 
+    /**
+     * Constructor 1.
+     *
+     * @param numerator   int.
+     * @param denominator int default is 1.
+     */
+    public Solution(int numerator, int denominator) {
+        if (denominator == 0) {
+            denominator = 1;
+        }
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    /**
+     * set Denominator for Fraction.
+     *
+     * @param denominator int not equal 0 and default = 1.
+     */
+    public void setDenominator(int denominator) {
+        if (denominator == 0) {
+            denominator = 1;
+        }
+        this.denominator = denominator;
+    }
+
+    /**
+     * reduce Fraction method.
+     *
+     * @return this Solution after reduce.
+     */
+    public Solution reduce() {
+        if (this.getNumerator() < 0) {
+            this.setNumerator(this.getNumerator() * (-1));
+        }
+        if (this.getDenominator() < 0) {
+            this.setDenominator(this.getDenominator() * (-1));
+        }
+        int g = 1;
+        for (int i = 2; i <= this.getNumerator() && i <= this.getDenominator(); i++) {
+            if (this.getNumerator() % i == 0 && this.getDenominator() % i == 0) {
+                g = i;
+            }
+        }
+        this.setNumerator(this.getNumerator() / g);
+        this.setNumerator(this.getDenominator() / g);
+        return this;
+    }
+
+    /**
+     * Add Fraction by Fraction.
+     *
+     * @param newSolution Solution .
+     * @return this Solution after adding.
+     */
+    public Solution add(Solution newSolution) {
+        this.setNumerator(this.getNumerator() * newSolution.getDenominator()
+                + this.getDenominator() * newSolution.getNumerator());
+        this.setDenominator(this.getDenominator() * newSolution.getDenominator());
+        return this;
+    }
+
+    /**
+     * Subtract Fraction by Fraction.
+     *
+     * @param newSolution Solution.
+     * @return this Solution after subtracting.
+     */
+    public Solution subtract(Solution newSolution) {
+        this.setNumerator(this.getNumerator() * newSolution.getDenominator()
+                - this.getDenominator() * newSolution.getNumerator());
+        this.setDenominator(this.getDenominator() * newSolution.getDenominator());
+        return this;
+    }
+
+    /**
+     * Multiply Fraction by Fraction.
+     *
+     * @param newSolution Solution.
+     * @return this Solution after multiplying.
+     */
+    public Solution multiply(Solution newSolution) {
+        this.setNumerator(this.getNumerator() * newSolution.getNumerator());
+        this.setDenominator(this.getDenominator() * newSolution.getDenominator());
+        return this;
+    }
+
+    /**
+     * Divide Fraction by Fraction.
+     *
+     * @param newSolution Solution.
+     * @return this Solution after divide.
+     */
+    public Solution divide(Solution newSolution) {
+        this.setNumerator(this.getNumerator() * newSolution.getDenominator());
+        this.setDenominator(this.getDenominator() * newSolution.getNumerator());
+        return this;
+    }
+
+    /**
+     * Check if 2 fractions are equal.
+     *
+     * @param obj Object .
+     * @return true if are equal.
+     * false else.
+     */
+    public boolean equals(Object obj) {
+        if (obj instanceof Solution) {
+            Solution other = (Solution) obj;
+            other.reduce();
+            Solution newSolution = new Solution(this.getNumerator(), this.getDenominator());
+            newSolution.reduce();
+            if (other.getNumerator() == newSolution.getNumerator()
+                    && other.getDenominator() == newSolution.getDenominator()) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+    }
 }
